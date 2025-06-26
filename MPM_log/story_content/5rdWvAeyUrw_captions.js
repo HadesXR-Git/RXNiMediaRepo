ccc8e4a9ccbc71dc1d35a2e4f556647e8a34fdf4
HadesXR_Win <66524055+HadesXR-Git@users.noreply.github.com>
2025-06-26 13:45:26 +0900

    MPM_log upload

---
+++ MPM_log/story_content/5rdWvAeyUrw_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.12.24693.0%0D%0A%0D%0A00:00:00.320%20--%3E%2000:00:04.644%0D%0ANew%20Achievement%20Unlocked!%0D%0A%0D%0A00:00:06.368%20--%3E%2000:00:07.456%0D%0A%0D%0A%0D%0A"}')
